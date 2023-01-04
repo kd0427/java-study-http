0298f445dff9c62857096eb238199f2bb91fcabc
kd0427 <rlaemek@naver.com>
2023-01-04 23:57:33 +0900

    s

---
+++ src/main/java/studyDay2/Ch09.java
@@ -0,0 +1,13 @@
+package studyDay2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ch09 {
+    public static void main(String[] args) {
+        List<String> arr = new ArrayList<>();
+        arr.add("한양아이클래스");
+        arr.add("김한양");
+        arr.forEach(a-> System.out.println());
+    }
+}
